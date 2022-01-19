0ead66698b9fbb9ffd721477e2f5e340c5bf2907
junho123 <wn531300@gmail.com>
2022-01-19 14:54:47 +0900

    add remark and modifiied css

--- version 1.0.2/main.js
+++ version 1.0.2/main.js
@@ -1,25 +1,21 @@
-"use strict";
+'use strict';
 
-const input = document.querySelector(".footer__input");
-const btn = document.querySelector(".footer__btn");
-const items = document.querySelector(".items");
-const form = document.querySelector(".new-form");
+const input = document.querySelector('.footer__input');
+const btn = document.querySelector('.footer__btn');
+const items = document.querySelector('.items');
+const form = document.querySelector('.new-form');
 
 let id = 0;
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
   onAdd();
 });
 
-/**입력한 내용이 리스트에 추가되어 보여진다
- *
- * @param
- * @returns
- */
+// 입력한 내용이 리스트에 추가되어 보여진다
 function onAdd() {
   const text = input.value;
-  if (text === "") {
+  if (text === '') {
     input.focus();
     return;
   }
@@ -27,20 +23,16 @@ function onAdd() {
   const item = createItem(text);
   items.appendChild(item);
 
-  input.value = "";
+  input.value = '';
   input.focus();
-  item.scrollIntoView({ block: "center" });
+  item.scrollIntoView({ block: 'center' });
 }
 
-/**새로운 아이템을 생성 (텍스트 + 삭제 버튼)
- *
- * @param {String} text 입력 받은 텍스트
- * @returns 리스트에 들어갈 li 아이템
- */
+// 새로운 아이템을 생성 (텍스트 + 삭제 버튼)
 function createItem(text) {
-  const itemRow = document.createElement("li");
-  itemRow.setAttribute("class", "item__row");
-  itemRow.setAttribute("data-id", id);
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
+  itemRow.setAttribute('data-id', id);
   itemRow.innerHTML = `
     <div class="item">
       <span class="item__name">${text}</span>
@@ -55,7 +47,8 @@ function createItem(text) {
   return itemRow;
 }
 
-items.addEventListener("click", (event) => {
+// 쓰레기통 버튼 클릭시 해당 id의 row 삭제
+items.addEventListener('click', (event) => {
   const id = event.target.dataset.id;
   if (id) {
     const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
